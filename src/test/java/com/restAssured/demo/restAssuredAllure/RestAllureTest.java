4a418b4212b7cbb51d7ab155672f1eddb54b81c9
Mukul Sehra <mukul.sehra@gmail.com>
2022-02-24 16:32:06 +0530

    added one positive and one negative test to check the allure report generation in case of 1 fail test and 1 pass

--- src/test/java/com/restAssured/demo/restAssuredAllure/RestAllureTest.java
+++ src/test/java/com/restAssured/demo/restAssuredAllure/RestAllureTest.java
@@ -1,8 +1,10 @@
 package com.restAssured.demo.restAssuredAllure;
 
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertNotNull;
 
 import java.util.HashMap;
 
@@ -17,17 +19,29 @@ public class RestAllureTest {
 	String BaseURL = "https://restful-booker.herokuapp.com";
 	 
     @SuppressWarnings("unchecked")
-	@Test(description = "GET Request Operation")
+	@Test(description = "GET the first booking id from the BookingId LIST")
     @Story("GET Request for bookings")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description : Verify the booking id is not blank")
-    public void verifyUser() {
+    public void listFirstBookingId() {
  
       Response res = given().filter(new AllureRestAssured()).when().get(BaseURL + "/booking");
       res.then().log().all();
       String val = String.valueOf(((HashMap<Object, Object>)res.then().extract().body().jsonPath().getList("$").get(0)).get("bookingid"));
       System.out.println(val);
-      Assert.assertTrue(!val.isBlank());
+      Assert.assertTrue(val.isBlank());
+    }
+    
+    @Test(description = "GET Booking details corresponding to a booking id")
+    @Story("GET Request for booking details")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description : Verify the booking details are not null")
+    public void verifyBookingDetails() {
+      String bookingId = "3";
+      Response res = given().filter(new AllureRestAssured()).when().get(BaseURL + "/booking/" + bookingId);
+      res.then().log().all();
+      res.then().assertThat().statusCode(HttpStatus.SC_OK);
+      Assert.assertNotNull(res.then().extract().body().asString());
     }
 
 }
